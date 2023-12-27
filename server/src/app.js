f997d600650f5d869a12508929044cb8590c199c
faizan-20 <faizan.20a@gmail.com>
2023-12-27 13:25:57 +0530

    basic user setup + file upload setup

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,4 @@
-import { express } from "express";
+import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cookie-parser";
 
@@ -6,9 +6,13 @@ const app = express();
 
 app.use(cors());
 app.use(cookieParser());
-
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+//routes
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
 
 export { app };
--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,13 @@ dotenv.config({
     path: "./.env",
 });
 
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import { application } from "express";
 
 connectDB()
     .then(() => {
-        application.listen(process.env.PORT || 8000, () => {
+        app.listen(process.env.PORT || 8000, () => {
             console.log(`Server is running at port: ${process.env.PORT}`);
         });
     })
