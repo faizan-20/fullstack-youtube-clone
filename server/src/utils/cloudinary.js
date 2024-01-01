67e2bcb122e7aba9ef67a1aaae10ea551a6daf2b
faizan-20 <faizan.20a@gmail.com>
2024-01-01 21:21:42 +0530

    fixed cloudinary upload errors

--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -1,5 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./.env",
+});
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -15,7 +19,8 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto",
         });
         // file has been uploaded succesfully
-        console.log("File uploaded on cloudinary: ", response.url);
+        //console.log("File uploaded on cloudinary: ", response.url);
+        fs.unlinkSync(localFilePath);
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath); //remove the local file from server if upload fails
